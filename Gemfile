cbca1dcff7c6259a23a8f4552b15b82c0533c42b
Bernardo Galindo <bernardo466@gmail.com>
2012-06-03 01:14:06 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'rails', '~> 3.2.5'
+gem 'rack'
+gem 'yajl-ruby', require: 'yajl'
+
+gem 'jquery-rails'
+gem 'copycopter_client', '2.0.0'
+
+
+# The database
+gem 'pg'
+
+# user
+gem 'devise'
+gem 'devise-encryptable' 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dogy::Application.config.secret_token = '42bb72395cf22a5477f82614af4397ceeaca1cecafa774cd8604672c0ecc35f1f583ea300089a42d12d4de5f2182dd8a79bfe5f90d273ca62fa6bce50d4edf12'
